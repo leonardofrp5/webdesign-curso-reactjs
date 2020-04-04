2fd51292d42ed018de1aebfe2c20ad0c74bf1087
leonardofrp5 <leonardofrp5@gmail.com>
2020-04-04 16:45:04 -0500

    I deleted component App and I added new component Login whit css, constsants and image, I created structure of file in Login

---
+++ src/screen/Login/constants.js
@@ -0,0 +1,14 @@
+export const INPUT_LOGIN = [
+  {
+    key: 1,
+    name: 'email',
+    type: 'email',
+    label: 'Correo electronico'
+  },
+  {
+    key: 2,
+    name: 'password',
+    type: 'password',
+    label: 'Contraseña'
+  }
+]
---
+++ src/screen/Login/index.js
@@ -0,0 +1,32 @@
+import React, { Component } from 'react';
+import { ReactComponent as UsuarioLogin } from '../../assets/usuario.svg';
+
+import { INPUT_LOGIN } from './constants';
+import './style.css';
+
+class Login extends Component {
+  store = {}
+
+  renderItem = (item) => {
+    return (
+      <div key={item.key} className="content-input">
+        <label className="text-label">{item.label}:</label>
+        <input type={item.type} name={item.name} className="decoration-input"/>
+      </div>
+    );
+  };
+
+  render() {
+    return (
+      <div className="from-container">
+        <UsuarioLogin className="image-login"/>
+        <form>
+        {INPUT_LOGIN.map(this.renderItem)}
+          <input type="submit" value="Ingresar"  className="btn-login"/>
+        </form>
+      </div>
+    )
+  }
+}
+
+export default Login
