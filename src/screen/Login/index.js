b8fb746c3dbd68f042167717e08ca2a998c16615
leonardofrp5 <leonardofrp5@gmail.com>
2020-04-19 16:07:30 -0500

    use state

--- src/screen/Login/index.js
+++ src/screen/Login/index.js
@@ -1,37 +1,25 @@
-import React from 'react';
-import { ReactComponent as UsuarioLogin } from '../../assets/usuario.svg';
+import React, { Component } from 'react';
+// import { ReactComponent as UsuarioLogin } from '../../assets/usuario.svg';
 
-import { INPUT_LOGIN } from './constants';
+// import { INPUT_LOGIN } from './constants';
 import './style.css';
 
-// class Login extends Component {
-//   store = {}
+class Login extends Component {
+  state = {
+    count: 0
+  }
 
-//   renderItem = (item) => {
-//     return (
-//       <div key={item.name} className="content-input">
-//         <label className="text-label">{item.label}:</label>
-//         <input type={item.type} name={item.name} className="decoration-input"/>
-//       </div>
-//     );
-//   };
+  handleOnClickPlus = () => {
+    const { count } = this.state;
+    this.setState({ count: count + 1 })
+  }
 
-//   render() {
-//     return (
-//       <div className="from-container">
-//         <UsuarioLogin className="image-login"/>
-//         <form>
-//         {INPUT_LOGIN.map(this.renderItem)}
-//           <input type="submit" value="Ingresar"  className="btn-login"/>
-//         </form>
-//       </div>
-//     )
-//   }
-// }
+  handleOnClickLess = () => {
+    const { count } = this.state;
+    this.setState({ count: count - 1 })
+  }
 
-function Login() {
-
-  const renderItem = (item) => {
+  renderItem = (item) => {
     return (
       <div key={item.name} className="content-input">
         <label className="text-label">{item.label}:</label>
@@ -40,15 +28,45 @@ function Login() {
     );
   };
 
-  return (
-    <div className="from-container">
-      <UsuarioLogin className="image-login"/>
-      <form>
-      {INPUT_LOGIN.map(renderItem)}
-        <input type="submit" value="Ingresar"  className="btn-login"/>
-      </form>
-    </div>
-  )
+  render() {
+    const { count } = this.state;
+    return (
+      // <div className="from-container">
+      //   <UsuarioLogin className="image-login"/>
+      //   <form>
+      //   {INPUT_LOGIN.map(this.renderItem)}
+      //     <input type="submit" value="Ingresar"  className="btn-login"/>
+      //   </form>
+      // </div>
+      <div className="from-container">
+        <button type="button" className="btn-login" onClick={this.handleOnClickPlus}>Sumar</button>
+        <span className="count">{count}</span>
+        <button type="button" className="btn-login" onClick={this.handleOnClickLess}>Restar</button>
+      </div>
+    )
+  }
 }
+
+// function Login() {
+
+//   const renderItem = (item) => {
+//     return (
+//       <div key={item.name} className="content-input">
+//         <label className="text-label">{item.label}:</label>
+//         <input type={item.type} name={item.name} className="decoration-input"/>
+//       </div>
+//     );
+//   };
+
+//   return (
+//     <div className="from-container">
+//       <UsuarioLogin className="image-login"/>
+//       <form>
+//       {INPUT_LOGIN.map(renderItem)}
+//         <input type="submit" value="Ingresar"  className="btn-login"/>
+//       </form>
+//     </div>
+//   )
+// }
 
 export default Login
